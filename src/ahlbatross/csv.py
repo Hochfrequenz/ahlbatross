7afb60ec08e8772bc89511aced9104b5e5bbb7cf
Konstantin <konstantin.klein+github@hochfrequenz.de>
2024-11-19 14:52:30 +0100

    chore: move `get_pruefid_files` to csv module

--- src/ahlbatross/csv.py
+++ src/ahlbatross/csv.py
@@ -15,3 +15,12 @@ def _get_csv_content(previous_ahb_path: Path, subsequent_ahb_path: Path) -> tupl
     previous_ahb: DataFrame = pd.read_csv(previous_ahb_path, dtype=str)
     subsequent_ahb: DataFrame = pd.read_csv(subsequent_ahb_path, dtype=str)
     return previous_ahb, subsequent_ahb
+
+
+def _get_pruefid_files(csv_dir: Path) -> list[Path]:
+    """
+    get all ahb/<pruefid>.csv files in a given directory.
+    """
+    if not csv_dir.exists():
+        return []
+    return sorted(csv_dir.glob("*.csv"))
